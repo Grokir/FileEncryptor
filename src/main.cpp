4f660fe9595579f167a4cbaee8b8c40a7b927bc8
Grokir <grokir13@yandex.ru>
2025-08-11 02:13:26 +0300

    Add parser of args

--- src/main.cpp
+++ src/main.cpp
@@ -1,210 +1,95 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-#include "DES/des.hpp"
-#include "DESX/desx.hpp"
-#include "FileRead/file_reader.hpp"
+#include "Interface/iface.hpp"
 
 using namespace std;
 
-template <typename ENCRYPTOR>
-void read_block  (ENCRYPTOR& encr, ifstream& inf );
 
-template <typename ENCRYPTOR>
-void write_block (ENCRYPTOR& encr, ofstream& outf);
+string help(){
+  return 
+  R"(
+    -h, --help                        help message
+    --des                             DES alg
+    --desx                            DESX alg
+    -f, --file <path to file>         Selected file for work
+    -d, --dir  <path to dir>          Selected dir for work
+    -E, --encrypt                     Encryption
+    -D, --decrypt                     Decryption
+  )";
+    // -k, --key  <path to key-file>     Selected key
+  };
+
+int main(int argc, char** argv) {  
+  vector<string> args(argc);
+  Operation oper;
+  vector<CFile> files;
+
+  for(int i = 0; i < argc; i++)
+    args[i] = string(argv[i]);
 
 
-template <typename ENCRYPTOR>
-int encryption  (ENCRYPTOR& encr, int cnt_iter, ifstream& inf, ofstream& outf);
-
-template <typename ENCRYPTOR>
-int decryption  (ENCRYPTOR& encr, int cnt_iter, ifstream& inf, ofstream& outf);
-
-uint calc_count_iteration(uint file_bitsize, uint block_bitsize);
-
-void DES_ALG ();
-void DESX_ALG();
-
-int main(int argc, char** argv) {
-  if(argc < 2){
-    cout << "[!] Invalid num of agruments!" << endl;
-    return -3;
+  if( argc < 2 || 
+      std::count(args.begin(), args.end(), "-h") || 
+      std::count(args.begin(), args.end(), "--help")
+    ){
+    cout << help() << endl;
+    return 1;
   }
+
+
+  if(
+      std::count(args.begin(), args.end(), "-E") ||
+      std::count(args.begin(), args.end(), "--encrypt")
+  )
+    oper = Operation::ENCR;
+  else if(
+      std::count(args.begin(), args.end(), "-D") ||
+      std::count(args.begin(), args.end(), "--decrypt")
+  )
+    oper = Operation::DECR;
+
+
+  if(
+    std::count(args.begin(), args.end(), "-f") ||
+    std::count(args.begin(), args.end(), "--file")
+  ){
+    int arg_f_pos = get_pos_elem(args, "-f");
+    int arg_file_pos = get_pos_elem(args, "--file");
+    
+    string path = args[max(arg_f_pos, arg_file_pos) + 1];
+    files.push_back(CFile(path));
+  }
+
   
-  if(string(argv[1]) == "--des")
-    DES_ALG();
-  else if(string(argv[1]) == "--desx")
-    DESX_ALG();
+  if(
+    std::count(args.begin(), args.end(), "-d") ||
+    std::count(args.begin(), args.end(), "--dir")
+  ){
+    int arg_f_pos = get_pos_elem(args, "-d");
+    int arg_file_pos = get_pos_elem(args, "--dir");
+    
+    string dir = args[max(arg_f_pos, arg_file_pos) + 1];
+    
+    files = fr::get_file_list(dir);
+  }
+
+
+
+  if(std::count(args.begin(), args.end(), "--des"))
+    DES_ALG(files, oper);
+  else if(std::count(args.begin(), args.end(), "--desx"))
+    DESX_ALG(files, oper);
   else
     cout << "[!] Error algs flag:\n" 
          << "     --des \n"   
          << "     --desx\n";
 
 
+  std::cout << "[+] Process: DONE" << std::endl;
+
   return 0;
 };
 
-
-template <typename ENCRYPTOR>
-int encryption  (ENCRYPTOR& encr, int cnt_iter, ifstream& inf, ofstream& outf){
-  for(uint i = 0; i < cnt_iter; i++){
-    read_block(encr, inf);
-    if(encr.encrypt())
-      write_block(encr, outf);
-    else
-      return -1;
-  }
-  return 0;
-};
-
-template <typename ENCRYPTOR>
-int decryption  (ENCRYPTOR& encr, int cnt_iter, ifstream& inf, ofstream& outf){
-  for(uint i = 0; i < cnt_iter; i++){
-    read_block(encr, inf);
-    if(encr.decrypt())
-      write_block(encr, outf);
-    else
-      return -1;
-  }
-  return 0;
-};
-
-
-
-template <typename ENCRYPTOR>
-void read_block  (ENCRYPTOR& encr, ifstream& inf ){
-  string str;
-  char buff;
-
-  for(int i = 0; i < encr.countPlainTextSymbols(); ++i){
-    inf.get(buff);
-
-    if(!inf.eof())
-      str += buff;
-  }
-  
-  encr.setMSG(str);
-  str.clear();
-};
-
-template <typename ENCRYPTOR>
-void write_block (ENCRYPTOR& encr, ofstream& outf){
-  outf.write(encr.getMSG().c_str(), encr.countPlainTextSymbols());
-};
-
-uint calc_count_iteration(uint file_bitsize, uint block_bitsize){
-  uint cnt_iter = file_bitsize / block_bitsize;
-
-  if(file_bitsize % block_bitsize != 0)
-    ++cnt_iter;
-
-  return cnt_iter;
-};
-
-
-void DES_ALG (){
-  DES       encryptor = DES();
-  string    key, file_name;
-  CFile     file;
-  uint      cnt_iter; 
-  ifstream  infile;
-  ofstream  outfile;
-
-  cout  << "[*] Enter key phrase (key should have length is 8 symbols): ";
-  cin   >> key;
-
-  encryptor.setKEY(key);
-
-  cout  << "[*] Enter path to file: ";
-  cin   >> file_name;
-
-  file.   init( file_name                       );
-  infile. open( file_name,           ios::binary);
-
-  cout  << "[!] File size " << file.get_size() << " bytes\n";
-
-  cnt_iter =  calc_count_iteration(
-                file.get_bit_size(), 
-                encryptor.countPlainTextBits()
-              );
-
-  char cmd;
-  cout << "[*] Enter what do: (e)ncrtyption or (d)ecryption?  ";
-  cin >> cmd;
-  
-  switch (cmd){
-    case 'e':
-      outfile.open(("ENC_" + file_name), ios::binary);
-      if(encryption(encryptor, cnt_iter, infile, outfile) < 0)
-        cout << "[!] Error encryption \n\n";
-      break;
-
-    case 'd':
-      outfile.open(("DEC_" + file_name), ios::binary);
-      if(decryption(encryptor, cnt_iter, infile, outfile) < 0)
-        cout << "[!] Error decryption \n\n";
-      break;
-  }
-  
-
-  infile. close();
-  outfile.close();
-
-  cout << "[+] Process: DONE" << endl;
-};
-void DESX_ALG(){
-  DESX      encryptor = DESX();
-  string    key, key1, key2, file_name;
-  CFile     file;
-  uint      cnt_iter; 
-  ifstream  infile;
-  ofstream  outfile;
-
-  cout  << "[*] Enter key  phrase (key should have length is 8 symbols): ";
-  cin   >> key;
-  cout  << "[*] Enter key1 phrase (key should have length is 8 symbols): ";
-  cin   >> key1;
-  cout  << "[*] Enter key2 phrase (key should have length is 8 symbols): ";
-  cin   >> key2;
-
-  encryptor.setKEY (key );
-  encryptor.setKEY1(key1);
-  encryptor.setKEY2(key2);
-
-  cout  << "[*] Enter path to file: ";
-  cin   >> file_name;
-
-  file.   init( file_name                       );
-  infile. open( file_name,           ios::binary);
-
-  cout  << "[!] File size " << file.get_size() << " bytes\n";
-
-  cnt_iter =  calc_count_iteration(
-                file.get_bit_size(), 
-                encryptor.countPlainTextBits()
-              );
-
-  char cmd;
-  cout << "[*] Enter what do: (e)ncrtyption or (d)ecryption?  ";
-  cin >> cmd;
-  
-  switch (cmd){
-    case 'e':
-      outfile.open(("ENC_" + file_name), ios::binary);
-      if(encryption(encryptor, cnt_iter, infile, outfile) < 0)
-        cout << "[!] Error encryption \n\n";
-      break;
-
-    case 'd':
-      outfile.open(("DEC_" + file_name), ios::binary);
-      if(decryption(encryptor, cnt_iter, infile, outfile) < 0)
-        cout << "[!] Error decryption \n\n";
-      break;
-  }
-  
-
-  infile. close();
-  outfile.close();
-
-  cout << "[+] Process: DONE" << endl;
-};
